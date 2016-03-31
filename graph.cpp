9afd0494000659c05e842b13bbf2535b4862baad
zhangheng <304544807@qq.com>
2016-03-31 17:25:28 +0800

    graph thery introduction

---
+++ graph.cpp
@@ -0,0 +1,30 @@
+int m = 2000,n = 1000;//边，节点
+
+int g[maxn][maxm];//临界矩阵
+g[u][v] = len
+
+//vector实现临界表
+struct node
+{
+	int to,len;
+};
+vector<node> G[maxn];
+g[i].push_back((node){to,len});
+
+//链式前向星
+strcut edgenode
+{
+	int to;
+	int len;
+	int next;
+}Edges[maxm];
+
+int head[maxn],k = 0;//节点头，边的编号
+
+void addedge(int i,int j,int len)
+{
+	Edges[k].to = j;
+	Edges[k].len = len;
+	Edges[k].next = head[i];
+	head[i] = k++;
+}
